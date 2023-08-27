ccf9b91b8ae137365877a26e48ef66b5528393fd
BizliCSE123 <khatunbizli892@gmail.com>
2023-08-27 21:16:44 +0600

    Add picture,context and read aloud

---
+++ story/lib/main.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() {
+  runApp(const PictureStoryApp());
+}
+
+class PictureStoryApp extends StatelessWidget {
+  const PictureStoryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Picture Story Time',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const StoryLibraryScreen(),
+    );
+  }
+}
+
+class StoryLibraryScreen extends StatelessWidget {
+  const StoryLibraryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Story Library'),
+      ),
+      body: ListView.builder(
+        itemCount: storyList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // Navigate to the story viewer screen
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => StoryViewerScreen(story: storyList[index]),
+                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Image.asset(
+                    storyList[index].coverImage,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 16.0),
+                  Text(
+                    storyList[index].title,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class StoryViewerScreen extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+
+  speak(String text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
+  }
+  final Story story;
+
+ StoryViewerScreen({super.key, required this.story});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(story.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              story.coverImage,
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                story.content,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => speak(story.content),
+              child: const Text('Read Aloud'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Story {
+  final String title;
+  final String coverImage;
+  final String content;
+
+  Story({required this.title, required this.coverImage, required this.content});
+}
+
+// Dummy data
+List<Story> storyList = [
+  Story(
+    title: 'The Adventure of Alex',
+    coverImage: 'assets/adventure.jpg',
+    content: 'The Adventures Of Alex: A Collection of Inspiring Stories about Courage, Friendship, Inner Strength and Self-Confidence by Arthur Martin. Adventure Tale of Professor Alex was commissioned in 2005 by the Japan Ground Self-Defense Force Eastern Army Band. Professor Alex is an imaginary character whose tales of adventure I have set to music. After studying as an archaeologist for 20 years, Professor Alex finds a mysterious map describing ancient treasures as well as an immortal.',
+  ),
+  // Add more stories here
+];
